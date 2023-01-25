55d0a5343801dcaa7389fcf576e1fe957e2677fa
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2023-01-25 09:51:10 +0000

    First version of pcost - file handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f) #skip the header
+    
+    for line in f:
+        stock = line.split(',')
+        nshares = int(stock[1])
+        price = float(stock[2])
+        total += (nshares * price)
+
+print(f'Total Cost {total}')

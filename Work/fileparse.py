5dbc5d056be0827cae7ff004ac9cda445b61b076
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2023-01-28 08:41:05 +0000

    fileparse up to exercise 3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,32 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=False):
+    '''Parse a CSV file into a list of records'''
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+        records = []
+
+        for row in rows:
+            if not row: #skip rows with no data
+                continue
+            if indices:
+                row = [ row[index] for index in indices ]
+            if types:
+                row = [f(entry) for f,entry in zip(types, row)]
+            
+            record = dict(zip(headers, row))
+            records.append(record)
+        
+    return records

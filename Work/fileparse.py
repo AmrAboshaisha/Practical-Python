4015ea86721b8a82cb4f858437400386e4413a93
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2023-01-28 09:01:30 +0000

    Finished Ex 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,29 +4,41 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=False):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''Parse a CSV file into a list of records'''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
-        # Read the file headers
-        headers = next(rows)
+        # Read the file headers (if exist)
+        if has_headers:
+            headers = next(rows)
+        
+        # If specific columns selected, get their indices to filter
         if select:
             indices = [headers.index(colname) for colname in select]
             headers = select
         else:
             indices = []
+
         records = []
 
         for row in rows:
             if not row: #skip rows with no data
                 continue
+                
+            # if specific columns have been selected, pick them via indices
             if indices:
                 row = [ row[index] for index in indices ]
+            
+            # apply type conversion to row if provided
             if types:
                 row = [f(entry) for f,entry in zip(types, row)]
-            
-            record = dict(zip(headers, row))
+
+            # Make dict or tuple 
+            if has_headers: 
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
         
     return records

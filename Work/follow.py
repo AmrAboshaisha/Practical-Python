28350a5e041466d71b875babdbfa071fb0007393
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2023-02-03 09:47:39 +0000

    Using Generator function to monitor data being written to file

---
+++ Work/follow.py
@@ -0,0 +1,25 @@
+# follow.py
+
+import os
+import time
+
+def follow(filename):
+    '''Generator function to reads lines being written to end of file'''
+    with open(filename, 'r') as f:
+        f.seek(0, os.SEEK_END) # Move file pointer 0 bytes from end of file
+        while True:
+            line = f.readline()
+            if line != '':
+                yield line
+            else:
+                time.sleep(0.1) #sleep breifly
+
+
+if __name__ == '__main__':
+    for line in follow('stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
